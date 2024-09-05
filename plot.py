d6ad91d88198bf4ac3b0c1ab8abbed64a985c6d7
s10210009 <s10210009@gmail.com>
2024-09-05 11:01:30 +0800

    　0905_add_exchange

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ t1=2
 t2=200000
 plt.figure()
 plt.plot(data[0,t1:t2],data[1,t1:t2], label='sx')
-plt.plot(data[0,t1:t2],data[4,t1:t2], label='analtic_sx')
+#plt.plot(data[0,t1:t2],data[4,t1:t2], label='analtic_sx')
 plt.legend()
 plt.xlabel('t')
 plt.savefig('spin_x.png')
@@ -16,7 +16,7 @@ plt.tight_layout()
 
 plt.figure()
 plt.plot(data[0,t1:t2],data[2,t1:t2], label='sy')
-plt.plot(data[0,t1:t2],data[5,t1:t2], label='analtic_sy')
+#plt.plot(data[0,t1:t2],data[5,t1:t2], label='analtic_sy')
 
 plt.legend()
 plt.xlabel('t')
@@ -25,19 +25,19 @@ plt.tight_layout()
 
 plt.figure()
 plt.plot(data[0,t1:t2],data[3,t1:t2], label='sz')
-plt.plot(data[0,t1:t2],data[6,t1:t2], label='analtic_sz')
+#plt.plot(data[0,t1:t2],data[6,t1:t2], label='analtic_sz')
 
 plt.legend()
 plt.xlabel('t')
 plt.savefig('spin_z.png')
 plt.tight_layout()
 
-plt.figure()
-plt.plot(data[0,t1:t2],data[1,t1:t2]-data[4,t1:t2], label='d_sx')
+#plt.figure()
+#plt.plot(data[0,t1:t2],data[1,t1:t2]-data[4,t1:t2], label='d_sx')
 #plt.plot(data[0,t1:t2],data[2,t1:t2]-data[5,t1:t2], label='d_sy')
-plt.plot(data[0,t1:t2],data[3,t1:t2]-data[6,t1:t2], label='d_sz')
-plt.legend()
-plt.xlabel('t')
-plt.savefig('error.png')
-plt.tight_layout()
-
+#plt.plot(data[0,t1:t2],data[3,t1:t2]-data[6,t1:t2], label='d_sz')
+#plt.legend()
+#plt.xlabel('t')
+#plt.savefig('error.png')
+#plt.tight_layout()
+#
--- spin.c
+++ spin.c
@@ -59,9 +59,9 @@ double thirty=30.0;
 //vector f(vector spin){
 //
 //}
-vector f(double fspinx, double fspiny, double fspinz,double dtime) {
+vector f(double fspinx, double fspiny, double fspinz,double dtime, vector H_eff) {
     vector fspin = {fspinx, fspiny, fspinz};
-    vector eq18_1 = crossProduct(fspin, H);
+    vector eq18_1 = crossProduct(fspin, H_eff);
     vector eq18_2 = crossProduct(scalarMultiply(fspin, lumba), eq18_1);
     vector d_spin = scalarMultiply(add(eq18_1, eq18_2), -1.0 * gama / (1.0 + lumba * lumba));
 //    fspin = add(fspin, scalarMultiply(d_spin, dtime));
@@ -116,11 +116,26 @@ void find_spin_neighbor(VectorArray* neighbor_index,int num_spin,vector* multi_s
     } else{
        neighbor_index[i].neighbors[3] = i+width;
     }
-//    printf("%d %d %d %d %d, neighbor index\n",i, neighbor_index[i].neighbors[0],neighbor_index[i].neighbors[1],neighbor_index[i].neighbors[2],neighbor_index[i].neighbors[3]);
+    printf("%d %d %d %d %d, neighbor index\n",i, neighbor_index[i].neighbors[0],neighbor_index[i].neighbors[1],neighbor_index[i].neighbors[2],neighbor_index[i].neighbors[3]);
   }
 }
+void calculate_H1_eff_app(vector* H_eff,VectorArray* neighbor_index,vector* multi_spin, int Nth_spin){
+   int neighbor1=neighbor_index[Nth_spin].neighbors[0];
+   int neighbor2=neighbor_index[Nth_spin].neighbors[1];
+   int neighbor3=neighbor_index[Nth_spin].neighbors[2];
+   int neighbor4=neighbor_index[Nth_spin].neighbors[3];
+   H_eff[Nth_spin].x =H.x ;
 
-void calculate_H_eff(vector* H_eff,VectorArray* neighbor_index,vector* multi_spin, int Nth_spin){
+   H_eff[Nth_spin].y =H.y ;
+
+   H_eff[Nth_spin].z =H.z ;
+
+//   printf("%d %d %d %d, neighbor index\n", neighbor1,neighbor2,neighbor3,neighbor4);
+//   H_eff.x = multi_spin[Nth_spin].x*(multi_spin[].x)
+
+}
+
+void calculate_H2_eff_exc(vector* H_eff,VectorArray* neighbor_index,vector* multi_spin, int Nth_spin){
    int neighbor1=neighbor_index[Nth_spin].neighbors[0];
    int neighbor2=neighbor_index[Nth_spin].neighbors[1];
    int neighbor3=neighbor_index[Nth_spin].neighbors[2];
@@ -128,16 +143,17 @@ void calculate_H_eff(vector* H_eff,VectorArray* neighbor_index,vector* multi_spi
    double Jxx=0.1;
    double Jyy=0.1;
    double Jzz=0.1;
-   H_eff[Nth_spin].x =H.x - Jxx* (multi_spin[Nth_spin].x*multi_spin[neighbor1].x + 
+   H_eff[Nth_spin].x += -Jxx* (multi_spin[Nth_spin].x*multi_spin[neighbor1].x + 
 	                          multi_spin[Nth_spin].x*multi_spin[neighbor2].x +
 			          multi_spin[Nth_spin].x*multi_spin[neighbor3].x +
 			          multi_spin[Nth_spin].x*multi_spin[neighbor4].x ) ;
-   H_eff[Nth_spin].y =H.y - Jyy* (multi_spin[Nth_spin].y*multi_spin[neighbor1].y +
+
+   H_eff[Nth_spin].y += -Jyy* (multi_spin[Nth_spin].y*multi_spin[neighbor1].y +
                                   multi_spin[Nth_spin].y*multi_spin[neighbor2].y +
                                   multi_spin[Nth_spin].y*multi_spin[neighbor3].y +
                                   multi_spin[Nth_spin].y*multi_spin[neighbor4].y ) ;
 	                    
-   H_eff[Nth_spin].z =H.z - Jzz* (multi_spin[Nth_spin].x*multi_spin[neighbor1].z +
+   H_eff[Nth_spin].z += -Jzz* (multi_spin[Nth_spin].x*multi_spin[neighbor1].z +
                                   multi_spin[Nth_spin].x*multi_spin[neighbor2].z +
                                   multi_spin[Nth_spin].x*multi_spin[neighbor3].z +
                                   multi_spin[Nth_spin].x*multi_spin[neighbor4].z ) ;
@@ -151,7 +167,7 @@ void calculate_H_eff(vector* H_eff,VectorArray* neighbor_index,vector* multi_spi
 int main(void)
 {
   double analytic_factor1 ,analytic_factor2;
-  int time_step = 2;//1000000;
+  int time_step = 1000000;
   int width=5, len=5;
   int num_spin = width*len;
   vector H_eff[num_spin];
@@ -171,10 +187,10 @@ int main(void)
   find_spin_neighbor(neighbor_index,num_spin, multi_spin_position, width,len);
   printf("%d  %d\n", neighbor_index[1].neighbors[0], neighbor_index[5].neighbors[3]);
   vector analytic_spin={1.0 , 0.0, 0.0};
-  vector rk1 ={0.0,0.0,0.0};
-  vector rk2 ={0.0,0.0,0.0};
-  vector rk3 ={0.0,0.0,0.0};
-  vector rk4 ={0.0,0.0,0.0};
+  vector rk1 [num_spin];
+  vector rk2 [num_spin];
+  vector rk3 [num_spin];
+  vector rk4 [num_spin];
 
   /////////////////////open file//////////////////////////////
   FILE *fptr;
@@ -188,29 +204,39 @@ int main(void)
   for(int i = 0; i < time_step; i++) {
     fprintf(fptr, "%.16f %.16f %.16f %.16f\n", dt*i, totalspin.x, totalspin.y, totalspin.z);
     totalspin.x=0.0,totalspin.y=0.0,totalspin.z=0.0;
-    
+
     for (int j = 0; j < num_spin; j++) {
-      calculate_H_eff(H_eff, neighbor_index, multi_spin, j);
-      printf("%.16f %.16f %.16f \n",H_eff[j].x, H_eff[j].y,H_eff[j].z);
-
-
-      rk1 = f(multi_spin[j].x, multi_spin[j].y, multi_spin[j].z, 0.0);
-      rk1 = scalarMultiply(rk1, dt);
-
-      rk2 = f(multi_spin[j].x + rk1.x/2.0, multi_spin[j].y + rk1.y/2.0, multi_spin[j].z + rk1.z/2.0, 0.5*dt);
-      rk2 = scalarMultiply(rk2, dt);
-
-      rk3 = f(multi_spin[j].x + rk2.x/2.0, multi_spin[j].y + rk2.y/2.0, multi_spin[j].z + rk2.z/2.0, 0.5*dt);
-      rk3 = scalarMultiply(rk3, dt);
-
-      rk4 = f(multi_spin[j].x + rk3.x, multi_spin[j].y + rk3.y, multi_spin[j].z + rk3.z, dt);
-      rk4 = scalarMultiply(rk4, dt);
-
-      multi_spin[j].x = multi_spin[j].x + (rk1.x + 2*rk2.x + 2*rk3.x + rk4.x)/6.0;
-      multi_spin[j].y = multi_spin[j].y + (rk1.y + 2*rk2.y + 2*rk3.y + rk4.y)/6.0;
-      multi_spin[j].z = multi_spin[j].z + (rk1.z + 2*rk2.z + 2*rk3.z + rk4.z)/6.0;
+      calculate_H1_eff_app(H_eff, neighbor_index, multi_spin, j);
+      calculate_H2_eff_exc(H_eff, neighbor_index, multi_spin, j);
+      //printf("%.16f %.16f %.16f \n",H_eff[j].x, H_eff[j].y,H_eff[j].z);
+      rk1[j] = f(multi_spin[j].x, multi_spin[j].y, multi_spin[j].z, 0.0,H_eff[j]);
+      rk1[j] = scalarMultiply(rk1[j], dt);
+    }
+    for (int j = 0; j < num_spin; j++) {
+      calculate_H1_eff_app(H_eff, neighbor_index, rk1, j);
+      calculate_H2_eff_exc(H_eff, neighbor_index, rk1, j);
+      rk2[j] = f(multi_spin[j].x + rk1[j].x/2.0, multi_spin[j].y + rk1[j].y/2.0, multi_spin[j].z + rk1[j].z/2.0, 0.5*dt,H_eff[j]);
+      rk2[j] = scalarMultiply(rk2[j], dt);
+    }
+    for (int j = 0; j < num_spin; j++) {
+      calculate_H1_eff_app(H_eff, neighbor_index, rk2, j);
+      calculate_H2_eff_exc(H_eff, neighbor_index, rk2, j);
+      rk3[j] = f(multi_spin[j].x + rk2[j].x/2.0, multi_spin[j].y + rk2[j].y/2.0, multi_spin[j].z + rk2[j].z/2.0, 0.5*dt,H_eff[j]);
+      rk3[j] = scalarMultiply(rk3[j], dt);
+    }
+    for (int j = 0; j < num_spin; j++) {
+      calculate_H1_eff_app(H_eff, neighbor_index, rk3, j);
+      calculate_H2_eff_exc(H_eff, neighbor_index, rk3, j);
+      rk4[j] = f(multi_spin[j].x + rk3[j].x, multi_spin[j].y + rk3[j].y, multi_spin[j].z + rk3[j].z, dt,H_eff[j]);
+      rk4[j] = scalarMultiply(rk4[j], dt);
+    }
+    for (int j = 0; j < num_spin; j++) {
+      multi_spin[j].x = multi_spin[j].x + (rk1[j].x + 2*rk2[j].x + 2*rk3[j].x + rk4[j].x)/6.0;
+      multi_spin[j].y = multi_spin[j].y + (rk1[j].y + 2*rk2[j].y + 2*rk3[j].y + rk4[j].y)/6.0;
+      multi_spin[j].z = multi_spin[j].z + (rk1[j].z + 2*rk2[j].z + 2*rk3[j].z + rk4[j].z)/6.0;
       multi_spin[j] = normalized(multi_spin[j]);
     }
+
     for (int j = 0; j < num_spin; j++) {
       totalspin.x+=multi_spin[j].x; totalspin.y+=multi_spin[j].y; totalspin.z+=multi_spin[j].z;
     }
